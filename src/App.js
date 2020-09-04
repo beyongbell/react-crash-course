b92b470487009316d4b03b2d704dc8d49f368091
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-04 11:04:51 +0700

    Prop Type Install

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Navbar from './Templates/Navbar';
 import Input  from './Components/Input';
 
+let id = 1;
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function addPost(title) {
+    const newPost = {id, title};
+    setPosts([newPost, ...posts])
+    id += 1;
+  }
+
   return (
     <div className="App">
       <Navbar/>
-      <Input />
+      <Input addPost={addPost} />
     </div>
   );
 }
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-export default function Input() {
+export default function Input({ addPost }) {
+    const [input, setInput] = useState('');
     return (
         <div className="Input">
             <div className="Input__header"> Create Post </div>
-            <input className="Input__field" type="text"/>
+            <input className="Input__field" type="text" value={input}/>
         </div>
     )
+}
+
+Input.propTypes = {
+    addPost: PropTypes.func.isRequired
 }
