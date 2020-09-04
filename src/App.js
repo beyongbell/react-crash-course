3abcc0c3b29b71098d679da5bce638740b01378a
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-04 15:42:32 +0700

    Finish Crash Course

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 import Navbar from './Templates/Navbar';
 import Input  from './Components/Input';
+import Post   from './Components/Post';
 
 let id = 1;
 
 function App() {
   const [posts, setPosts] = useState([]);
 
-  function addPost(title) {
-    const newPost = {id, title};
-    setPosts([newPost, ...posts])
+  function addPost(newPost) {
+    setPosts([{ id, title: newPost }, ...posts]);
     id += 1;
+  }
+
+  function removePost(id) {
+    const updatedPosts = posts.filter((post) => post.id !== id);
+    setPosts(updatedPosts);
   }
 
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Input addPost={addPost} />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          removePost={removePost}
+        />
+      ))}
     </div>
   );
 }
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,10 +3,23 @@ import PropTypes from 'prop-types'
 
 export default function Input({ addPost }) {
     const [input, setInput] = useState('');
+
+    function onChange(event) {
+        setInput(event.target.value);
+    }
+
+    function onKeyDown(event) {
+        const title = event.target.value;
+        if (event.key === 'Enter' && title) {
+            addPost(title);
+            setInput('');
+        }
+    }
+
     return (
         <div className="Input">
             <div className="Input__header"> Create Post </div>
-            <input className="Input__field" type="text" value={input}/>
+            <input className="Input__field" type="text" value={input} onChange={onChange} onKeyDown={onKeyDown}/>
         </div>
     )
 }
