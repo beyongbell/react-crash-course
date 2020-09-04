a3ea972c4e2f0dd0858253df1f6bb2dbb4a33d69
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-09-04 10:54:13 +0700

    Css Input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './Templates/Navbar';
+import Input  from './Components/Input';
+
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
+      <Input />
     </div>
   );
 }
---
+++ src/Components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Input() {
+    return (
+        <div className="Input">
+            <div className="Input__header"> Create Post </div>
+            <input className="Input__field" type="text"/>
+        </div>
+    )
+}
